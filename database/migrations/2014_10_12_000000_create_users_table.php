a5e945d1a79c4735ea0fadef44daff3128030491
Nipe Setiawan <nipeharefa@gmail.com>
2017-04-07 07:53:17 +0700

    before up API

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,9 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('phone')->nullable();
+            $table->string('picture_url')->nullable();
+            $table->integer('saldo')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
