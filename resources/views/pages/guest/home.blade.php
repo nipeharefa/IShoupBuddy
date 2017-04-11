e7e12d55940bc2026a58b5e5aa1ae0b0fd47c036
Nipe Setiawan <nipeharefa@gmail.com>
2017-04-11 16:51:12 +0700

    diff production css

--- resources/views/pages/guest/home.blade.php
+++ resources/views/pages/guest/home.blade.php
@@ -10,16 +10,30 @@
 @section('css')
 	
 	<link rel="stylesheet" href="{{ mix('css/guest/home.css') }}">
-	<link rel="stylesheet" href="{{ asset('local/swiper/swiper.min.css') }}">
+
+	@if (env('APP_ENV') == "production")
+		
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/css/swiper.min.css">
+	@else		
+		<link rel="stylesheet" href="{{ asset('local/swiper/swiper.min.css') }}">
+	@endif	
+
 
 @endsection
 
 @section('js')
 	
 	<script src="{{ mix('manifest.js') }}"></script>
-	<script src="{{ mix('js/guest/vendor.js') }}"></script>	
 
-	<script src="{{ asset('local/swiper/swiper.min.js') }}"></script>
+	@if (env('APP_ENV') == "production")
+		
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/js/swiper.min.js"></script>
+	@else		
+		<script src="{{ asset('local/swiper/swiper.min.js') }}"></script>
+	@endif	
+
+	<script src="{{ mix('js/guest/vendor.js') }}"></script>	
+	
 	<script src="{{ mix('js/guest/home.js') }}" type="text/javascript"></script>	
 
 @endsection
