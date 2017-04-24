52fdab79aa96c97a5951fa3886d232c9a3e53cfe
Nipe Setiawan <nipeharefa@gmail.com>
2017-04-24 20:11:32 +0700

    Add products

---
+++ app/Http/Controllers/API/ProductVendorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\ProductVendor;
+use Illuminate\Http\Request;
+
+class ProductVendorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ProductVendor  $productVendor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductVendor $productVendor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ProductVendor  $productVendor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductVendor $productVendor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ProductVendor  $productVendor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProductVendor $productVendor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ProductVendor  $productVendor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductVendor $productVendor)
+    {
+        //
+    }
+}
