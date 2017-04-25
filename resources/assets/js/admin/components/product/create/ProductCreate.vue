0ac6ecbf2ea3ae88b1abcd8cb00f44674c0318bb
Nipe Setiawan <nipeharefa@gmail.com>
2017-04-25 23:40:43 +0700

    add sidebar admins

--- resources/assets/js/admin/components/product/create/ProductCreate.vue
+++ resources/assets/js/admin/components/product/create/ProductCreate.vue
@@ -4,44 +4,62 @@
       <section class="section">
         <div class="container">
           <div class="columns">
+            <div class="column is-one-quarter">
+              <admin-sidebar></admin-sidebar>
+            </div>
+
             <div class="column">
+              <div class="columns">
 
-            <input type="file" class="input" @change="_uploadImage">
-            <div class="field">
-              <label class="label">Nama Produk</label>
-              <p class="control">
-                  <input type="text" class="input"  placeholder="Nama Produk" v-model="product.name"/>
-                </p>
-            </div>
+                <div class="column is-one-quarter">
+                  <div>
+                    <figure class="image is-4by3">
+                      <img :src="generateLinkImage(product.picture_url) || 'http://bulma.io/images/placeholders/256x256.png'">
+                    </figure>
+                    <input type="file" class="input" @change="_uploadImage">
+                  </div>
+                </div>
 
-            <div class="field">
-              <label class="label">Barcode</label>
-              <p class="control">
-                  <input type="text" class="input"  placeholder="Barcode" v-model="product.barcode"/>
-                </p>
-            </div>
+                <div class="column is-half">
+                  <div class="field">
+                    <label class="label">Nama Produk</label>
+                    <p class="control">
+                        <input type="text" class="input"  placeholder="Nama Produk" v-model="product.name"/>
+                      </p>
+                  </div>
 
-            <div class="field">
-              <label class="label">Category</label>
-              <p class="control">
-                <span class="select">
-                  <select name="" v-model="product.category_id" class="select">
-                    <option value="1">Uncategorized</option>
-                  </select>
-                </span>
-                </p>
-            </div>
+                  <div class="field">
+                    <label class="label">Barcode</label>
+                    <p class="control">
+                        <input type="text" class="input"  placeholder="Barcode" v-model="product.barcode"/>
+                      </p>
+                  </div>
 
-            <div class="field">
-              <label class="label">Message</label>
-              <p class="control">
-                <textarea class="textarea" placeholder="Textarea" v-model="product.description"></textarea>
-              </p>
-            </div>
+                  <div class="field">
+                    <label class="label">Kategori</label>
+                    <p class="control">
+                      <span class="select">
+                        <select name="" v-model="product.category_id" class="select">
+                          <option value="">Pilih Kategori</option>
+                          <option value="1">Uncategorized</option>
+                        </select>
+                      </span>
+                      </p>
+                  </div>
 
-            <div class="field">
-              <button class="button is-primary" @click="saveProduct">Simpan</button>
-            </div>
+                  <div class="field">
+                    <label class="label">Deskripsi</label>
+                    <p class="control">
+                      <textarea class="textarea" placeholder="Deskripsi produk" v-model="product.description"></textarea>
+                    </p>
+                  </div>
+
+                  <div class="field">
+                    <button class="button is-primary" @click="saveProduct">Simpan</button>
+                  </div>
+
+                </div>
+              </div>
 
             </div>
 
@@ -57,6 +75,9 @@
 
 <script>
   const FooterApps = () => import('otherComponents/Footer.vue')
+  const AdminSidebar = () => import('otherComponents/Sidebars/AdminSidebar.vue')
+  const FormAddProduct = () => import('adminProduct/create/FormAddProduct.vue')
+
   export default {
     data() {
 
@@ -67,13 +88,22 @@
           description: "-",
           category_id: "",
           barcode: ""
-        }
+        },
+        onProcess: false,
+        onError: false
       }
     },
     components: {
-      FooterApps
+      FooterApps,
+      FormAddProduct,
+      AdminSidebar
     },
     methods: {
+      generateLinkImage(filename) {
+        if (filename) {
+          return window.location.origin + '/image/small/' + filename
+        }
+      },
       _uploadImage (e) {
         const file = e.target.files[0]
         const fileName = file.name.toLowerCase()
