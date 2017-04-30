c38c73b1870f6506a265557b328355a118bcaac6
Nipe Setiawan <nipeharefa@gmail.com>
2017-04-30 22:20:44 +0700

    try to rewrite https

--- app/Helpers/Transformers/ProductTransformer.php
+++ app/Helpers/Transformers/ProductTransformer.php
@@ -6,7 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductTransformer extends AbstractTransformer {
 
+
     public function transformModel(Model $product){
+
+        $secure = env('APP_ENV') == "production";
 
         $arr = [
             "id"            =>  $product->id,
@@ -15,9 +18,9 @@ class ProductTransformer extends AbstractTransformer {
             "barcode"       =>  (double)$product->barcode,
             "picture_url"   =>
             [
-                "small"     =>  url('/image/small', $product->picture_url),
-                "medium"    =>  url('/image/medium', $product->picture_url),
-                "large"     =>  url('/image/large', $product->picture_url),
+                "small"     =>  url('/image/small', $product->picture_url, $secure),
+                "medium"    =>  url('/image/medium', $product->picture_url, $secure),
+                "large"     =>  url('/image/large', $product->picture_url, $secure),
             ],
             "description"   =>  "",
             "vendors"       =>  ProductVendorTransformer::transform($product->ProductVendor),
