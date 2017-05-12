1e41918ea5c5249228fe055df85f2978eb4ddc27
Nipe Setiawan <nipeharefa@gmail.com>
2017-05-12 23:58:42 +0700

    fix private function

--- app/Helpers/Contracts/Sentimen.php
+++ app/Helpers/Contracts/Sentimen.php
@@ -8,7 +8,5 @@ interface Sentimen {
     public function score($sentence);
     public function categorise($sentence);
     public function getList($type);
-    private function _cleanString($string);
-    private function loadDefaults();
     public function setDictionary();
 }
