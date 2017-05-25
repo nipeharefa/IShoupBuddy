69e57cc336e31c5574dbd35add5546e0a2e4a001
Nipe Setiawan <nipeharefa@gmail.com>
2017-05-26 01:15:03 +0700

    add modals to cart

---
+++ resources/assets/js/global/components/Cart/ModalAddProductToCart.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="modal modal-product-to-cart">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <!-- Any other Bulma elements you want -->
+    </div>
+    <button class="modal-close"></button>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    mounted () {}
+  }
+</script>
