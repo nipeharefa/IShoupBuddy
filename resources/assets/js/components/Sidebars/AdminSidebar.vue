cc853542b6b366a26106ee082c7e0ebaa5db4f8a
Nipe Setiawan <nipeharefa@gmail.com>
2017-06-08 12:21:23 +0700

    add review link, upgrade vue

--- resources/assets/js/components/Sidebars/AdminSidebar.vue
+++ resources/assets/js/components/Sidebars/AdminSidebar.vue
@@ -4,16 +4,15 @@
       General
     </p>
     <ul class="menu-list">
-      <!-- <li><a>Dashboard</a></li> -->
-      <!-- <li><a href="/admin/product">Products</a></li> -->
       <li>
         <router-link :to="{ name: 'listProducts' }" append>Products</router-link>
       </li>
       <li>
+        <router-link :to="{ name: 'listProducts' }" append>Review</router-link>
+      </li>
+      <li>
         <router-link :to="{ name: 'listTransaction' }" append>Transactions</router-link>
       </li>
-      <!-- <li><a href="/admin/vendor">Users</a></li>
-      <li><a href="/admin/vendor">Vendors</a></li> -->
       <li>
         <router-link :to="{ name: 'listVendor' }" append>Vendors</router-link>
       </li>
