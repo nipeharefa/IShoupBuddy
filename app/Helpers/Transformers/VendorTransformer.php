3a87a7576ef3a8b59772047b6dbcd9df782109fa
Nipe Setiawan <nipeharefa@gmail.com>
2017-06-20 22:23:22 +0700

    space

--- app/Helpers/Transformers/VendorTransformer.php
+++ app/Helpers/Transformers/VendorTransformer.php
@@ -9,10 +9,10 @@ class VendorTransformer extends AbstractTransformer {
     public function transformModel(Model $vendor){
 
         $arr = [
-            "id"    =>  $vendor->id,
-            "name"  =>  $vendor->name,
-            "email" =>  $vendor->email,
-            "confirmed" =>  (Boolean) $vendor->confirmed,
+            "id"            =>  $vendor->id,
+            "name"          =>  $vendor->name,
+            "email"         =>  $vendor->email,
+            "confirmed"     =>  (Boolean) $vendor->confirmed,
             "picture_url"   =>  $vendor->picture_url,
             "total_product" =>  $vendor->ProductVendor()->count(),
             "total_review"  =>  0,
