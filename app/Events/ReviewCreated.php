99f827a900170103e938cfa43a612b853b9415fc
Nipe Setiawan <nipeharefa@gmail.com>
2017-06-30 23:43:44 +0700

    add create and update events

---
+++ app/Events/ReviewCreated.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use App\Models\Review;
+use App\Models\User;
+
+class ReviewCreated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    protected $product;
+
+    protected $user;
+
+    public $review;
+
+    public $summaryProductVendor;
+
+    public $summaryProduct;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
