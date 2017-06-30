070f42f02dcbe1aa0e190c45e197ec6d861cd506
Nipe Setiawan <nipeharefa@gmail.com>
2017-06-30 15:29:42 +0700

    add events saldo nominal

---
+++ app/Events/SaldoNominalUpdated.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use App\Models\Saldo;
+use App\Models\User;
+use App\Helpers\Traits\RupiahFormated;
+
+class SaldoNominalUpdated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels, RupiahFormated;
+
+
+    public $saldo = [];
+
+    protected $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+        $this->preNominal($user->Saldo);
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('saldo.user.'.$this->user->id);
+    }
+
+    public function broadcastAs()
+    {
+        return 'saldo.updated';
+    }
+
+    protected function preNominal(Saldo $saldo)
+    {
+        $saldo = $saldo->nominal ?? 0;
+
+        $response = [
+            "message"       =>  null,
+            "saldo"         =>  $saldo,
+            "saldo_string"  =>  $this->formatRupiah($saldo),
+            "status"        =>  "OK"
+        ];
+
+        $this->saldo = $response;
+    }
+}
