df419bc37d4710516fe8a59203ec36b3e70368fc
Nipe Setiawan <nipeharefa@gmail.com>
2017-07-04 00:24:48 +0700

    change user_name to a

--- resources/assets/js/global/components/Navbars/partials/RightMenuMember.vue
+++ resources/assets/js/global/components/Navbars/partials/RightMenuMember.vue
@@ -6,8 +6,8 @@
       <div class="user_info">
         <a class="nav-item" href="/me">
           <i class="fa fa-user" aria-hidden="true"></i>
+          <span class="user_name">Hi, {{activeUser.name}}</span>
         </a>
-        <span class="user_name">Hi, {{activeUser.name}}</span>
       </div>
   </div>
 </template>
@@ -15,6 +15,7 @@
 <style lang="scss" scoped>
   span.user_name {
     font-size: 0.8rem;
+    margin-left: 0.4rem;
   }
 </style>
 
