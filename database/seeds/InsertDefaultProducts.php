877c837129f6e4cd0bf5a3ca58b8fb914511a0fe
Nipe Setiawan <nipeharefa@gmail.com>
2017-07-11 12:08:26 +0700

    fix default Products

--- database/seeds/InsertDefaultProducts.php
+++ database/seeds/InsertDefaultProducts.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\Models\Category;
 use App\Models\Product;
+use Faker\Factory as Faker;
 
 class InsertDefaultProducts extends Seeder
 {
@@ -13,20 +14,24 @@ class InsertDefaultProducts extends Seeder
      */
     public function run()
     {
+        $faker = Faker::create();
         $category = Category::first();
 
         $dat = Storage::get('init-products.json');
         $data = json_decode($dat, true);
 
+        DB::statement("SET foreign_key_checks=0");
+        Product::truncate();
+        DB::statement("SET foreign_key_checks=1");
 
-        $beforeProduct = collect($data)->map(function ($item) use ($category) {
+        $beforeProduct = collect($data)->map(function ($item) use ($category, $faker) {
             return [
                 "name"  =>  $item["name"],
                 "category_id"   =>  $category->id,
-                "barcode"       =>  $item['barcode'],
+                "barcode"       =>  $item['barcode'] ?? $faker->ean13,
                 "slug"          =>  str_slug($item['name']),
                 "picture_url"   =>  $item['picture_url'],
-                "description"   =>  "-"
+                "description"   =>  $faker->realText
             ];
         });
 
