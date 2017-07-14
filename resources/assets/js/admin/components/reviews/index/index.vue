6bd207fc5cc9ca0deb4d0d64bb3b130e61347d8c
Nipe Setiawan <nipeharefa@gmail.com>
2017-07-14 12:12:32 +0700

    fix search reviews

--- resources/assets/js/admin/components/reviews/index/index.vue
+++ resources/assets/js/admin/components/reviews/index/index.vue
@@ -5,15 +5,27 @@
       <ul>
         <li><a href="/">Halaman Depan</a></li>
         <li>
-          <router-link :to="{ name: 'summaryProfile' }" append>
+          <router-link :to="{ name: 'dashboard' }" append>
             Administrator
           </router-link>
         </li>
         <li class="is-active"><a>Review</a></li>
       </ul>
     </nav>
+    <div class="review_search__wrapper columns">
+      <div class="field column is-6 has-addons">
+        <p class="control">
+          <input class="input" type="text" placeholder="Find a Review" v-model="keyword">
+        </p>
+        <p class="control">
+          <a class="button is-info">
+            <i class="fa fa-search"></i>
+          </a>
+        </p>
+      </div>
+    </div>
     <div class="review__wrapper columns">
-      <review v-for="item in reviews" :review="item" :key="item.id" />
+      <review v-for="item in reviewFilterd" :review="item" :key="item.id" />
     </div>
   </div>
 </template>
@@ -29,7 +41,23 @@
     computed: {
       ...mapGetters([
         'reviews'
-      ])
+      ]),
+      cleanKeyword () {
+        return this.keyword.replace(/[|&;$%@"<>()+,]/g, "")
+      },
+      reviewFilterd () {
+        if (this.reviews) {
+          return this.reviews.filter( x => {
+            return x.body.includes(this.cleanKeyword)
+          })
+        }
+        return null
+      }
     },
+    data () {
+      return {
+        keyword: ""
+      }
+    }
   }
 </script>
