a6047b84f4c2c54751736dc421a939f83a135ed4
Nipe Setiawan <nipeharefa@gmail.com>
2017-07-31 15:47:14 +0700

    use external bulma

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,6 +2,12 @@
 
 @section('css')
 
+    @if (env('APP_ENV') == "production")
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.0/css/bulma.min.css">
+    @else
+        <link rel="stylesheet" href="{{ asset('local/bulma/0.5.0/bulma.min.css') }}">
+    @endif
+
     <link rel="stylesheet" href="{{ mix('css/guest/register.css') }}">
 
 @endsection
